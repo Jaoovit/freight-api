703aba9f17272cc34bc44bb2553dab4ea9860539
João Oliveira <jaoovit@gmail.com>
2024-11-17 16:26:49 +0000

    define the initialization session using prisma

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 require("dotenv").config();
 const express = require("express");
+const initializeSession = require("./config/session");
 
 const app = express();
 
 // Middleware to parte JSON and URL-encoded data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// Prisma session configuration
+initializeSession(app);
 
 // Routes
 const userRoute = require("./routes/userRoute");
